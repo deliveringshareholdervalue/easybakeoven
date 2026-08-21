f08aadfccb14444b7519018e4b80dce550134a30
deliveringshareholdervalue <aidan.stoner27@gmail.com>
2026-08-21 16:44:24 -0700

    build out package list for deb-server.sh script
    added dependencies for docker and text editors into the script.

--- debian/deb-server.sh
+++ debian/deb-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# easybakeoven logo
 cat deb-art.txt
 echo
 echo
@@ -6,6 +7,7 @@ echo
 echo "Life is hard, but deploying debian doesn't have to be!"
 echo
 sleep 1
+# makes sure sudo is installed for execution of commands
 if ! command -v sudo &> /dev/null; then
 	echo "sudo is not installed, please install. exiting script" >&2
 	exit 1
@@ -21,8 +23,14 @@ while true; do
 			echo "Vamanos!"
 
 			sudo apt update
-
-			sudo apt install -y curl git openssh cockpit fastfetch openjdk-25-jdk wget tmux ufw btop build-essential 
+# installs all necessary dependencies with a yes check automatically so no authentication needed
+			sudo apt install -y curl git openssh cockpit fastfetch openjdk-25-jdk wget tmux ufw btop build-essential neovim nano ca-certificates gnupg
+			# installs docker keyring
+			sudo mkdir -p /etc/apt/keyrings
+			curl -fsSL https://docker.com | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+			sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# installs docker modules needed for function
+           sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 			break
 			;;
 		[Nn]* )
