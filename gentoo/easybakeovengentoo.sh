51641dcb6d219146741990f6f87c06a0052f11d6
Kelly Avila <kellyavila@Kellys-MacBook-Air.local>
2026-08-24 14:52:07 -0700

    rewrote code to not throw error code

--- gentoo/easybakeovengentoo.sh
+++ gentoo/easybakeovengentoo.sh
@@ -3,8 +3,14 @@ echo "Welcome to gentoo KDE script"
 echo
 echo "Launching"
 echo
-emerge -an kde-plasma/kdeplasma-addons
+sudo emerge -vkg kde-plasma/kdeplasma-addons
 echo
+sudo emerge -vkg kde-plasma/kdeplasma-addons
+
+if ! equery list kde-plasma/kdeplasma-addons >/dev/null 2>&1; then
+    echo "kde-plasma/kdeplasma-addons didn't install lol"
+    exit 1
+fi
 echo "Done!"
 
 done
