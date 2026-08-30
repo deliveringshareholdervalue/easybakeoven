df6c029fc25f6f5960dc25123fa3929552b9b828
deliveringshareholdervalue <aidan.stoner27@gmail.com>
2026-08-30 00:58:07 -0700

    push proxmox script until i can figure out what needs to go into it. updating git folder b4 i forget

---
+++ proxmox-ve/alpine/alpine-server.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+cat alpine-server.txt
+echo
+echo
+echo "Welcomne to Alpine-server install"
+
+
+# start installing stuff going to update this as packages are needed
+pkg install git
+
+exit 0
+
+
